1f39251950a051d624d8df6044e0f6eee229841d
Kastan Day <kastanvday@gmail.com>
2024-01-18 18:52:38 -0800

    add prints

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import { crawl } from './api/crawlee.js';
 import cors from 'cors';
 
 const app = express();
+
+// const corsOptions = {
+//     origin: 'https://uiuc.chat',
+// };
+
 app.use(cors()); // Enable CORS for all routes and origins
+// app.use(cors(corsOptions)); // only certain routes
 app.use(express.json());
 
 app.post('/crawl', async (req: Request, res: Response) => {
+    console.log('in /crawl. req.body:', req.body)
     try {
         const { url, match, maxPagesToCrawl, maxTokens, courseName } = req.body;
+        console.log('in /crawl -- got variables :) url:', url, 'match:', match, 'maxPagesToCrawl:', maxPagesToCrawl, 'maxTokens:', maxTokens, 'courseName:', courseName)
 
         const config = {
             url,
@@ -20,6 +28,7 @@ app.post('/crawl', async (req: Request, res: Response) => {
         };
 
         const results = await crawl(config);
+        console.log('in /crawl -- results:', results)
         res.status(200).json(results);
     } catch (error) {
         const e = error as Error;
