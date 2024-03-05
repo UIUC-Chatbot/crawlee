0917b39d3b6497ca6a8bff8995315748fa055a92
Kastan Day <kastanvday@gmail.com>
2024-03-05 11:36:56 -0800

    Try to fix cors by adding header

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,17 @@ const app = express();
 //     origin: 'https://uiuc.chat',
 // };
 
-app.use(cors()); // Enable CORS for all routes and origins
+// app.use(cors()); // Enable CORS for all routes and origins
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    } else {
+        next();
+    }
+});
 // app.use(cors(corsOptions)); // only certain routes
 app.use(express.json());
 
